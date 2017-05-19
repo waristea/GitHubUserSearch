54715c08de475e1f819a256d02892b398e201920
William Aristea Tantiono <waristea@gmail.com>
2017-05-19 22:38:59 +0700

    Parse JSONObject into JSONArray

--- src/githubusersearch/GithubUserSearch.java
+++ src/githubusersearch/GithubUserSearch.java
@@ -10,6 +10,7 @@ import java.nio.charset.Charset;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class GithubUserSearch {
 
@@ -33,10 +34,38 @@ public class GithubUserSearch {
       is.close();
     }
   }
+  
+  public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONArray json = new JSONArray(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
 
   public static void main(String[] args) throws IOException, JSONException {
-    JSONObject json = readJsonFromUrl("https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000&per_page=5&sort=stars&order=desc");
-    System.out.println(json.toString());
-    System.out.println(json.get("total_count"));
+    String url = "https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000&per_page=5&sort=stars&order=desc";
+    JSONObject json = readJsonFromUrl(url);
+    JSONArray userArray = json.getJSONArray("items");
+    
+    for(int i=0; i<userArray.length(); ++i){
+        JSONObject user = (JSONObject) userArray.get(i);
+        
+        String username = user.getString("login");
+        
+        String reposUrl = user.getString("repos_url").concat("?per_page=100");
+        System.out.println(reposUrl);
+        JSONArray repoArray = readJsonArrayFromUrl(reposUrl);
+        
+        System.out.println((i+1)+". "+username);
+        // Handle repos>100
+        // Reminder : Make if statement for empty string 
+        System.out.println("Num o' Repo : " + repoArray.length());
+        
+    }
   }
 }
